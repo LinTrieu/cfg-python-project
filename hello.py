bcfd2e1c684d3ce3d8df407a17a0d13410848f32
georgia.shaw <georgia@cranbrookw4.com>
2018-12-05 18:47:18 +0000

    Fixed everything

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,json
+from jsonreading import extract_json
 
 app = Flask(__name__)
 
@@ -10,19 +11,34 @@ def hello():
 
 
 
-@app.route('/confirmation', methods=["GET", "POST"])
+@app.route('/confirmation')
 def confirmation():
-   data = {}
+    with open("users.json", "r") as outfile:
+        raw_data = json.load(outfile)
+        print(raw_data)
+    new_user = {
+       "email": request.args.get('email'),
+       "first_name": request.args.get('firstname'),
+       "surname": request.args.get('lastname')
+    }
+    print(new_user)
 
-   data["users"] = []
-   data["users"].append({
-       "name": request.args.get('email'),
-       "address": request.args.get('firstname'),
-       "phone": request.args.get('lastname')
-   })
-   with open("users.json", "w") as write_file:
-       json.dump(data, write_file)
-   return render_template('confirmation.html')
+    with open("users.json", "w+") as outfile:
+        raw_data['users'].append(new_user)
+
+        json.dump(raw_data, outfile)
+    return render_template('confirmation.html')
+
+@app.route("/userprofile/<user_number>")
+def data(user_number):
+    raw_data = extract_json()
+    users = raw_data ["users"]
+    current_user = users[int(user_number)]
+    # return str(current_user)
+    email= str(current_user['email'])
+    first_name = str(current_user['firstname'])
+    surname = str(current_user['surname'])
+    return render_template("userprofile.html", email=email, first_name=first_name, surname=surname)
 
 
 app.run(debug=True)
