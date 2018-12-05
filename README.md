e48b422dc56807fe89b07b9d2c38a1e8f7871f17
soho <soho>
2018-12-05 08:51:32 +0000

    index page code

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-https://lintrieu.github.io/cfg-python-project/
-
-This is the project folder for the Code First: Girls python course competition (finishing 05/12/18)
-
-Team members: Lin Trieu, Georgia Shaw, Amber Rignell
+# pythoncompetitionproject
+#This is the project folder for the Code First: Girls python course competition (finishing 05/12/18)
+#Team members: Lin Trieu, Georgia Shaw, Amber Rignell
--- hello.py
+++ hello.py
@@ -1,23 +1,29 @@
-from flask import Flask, render_template, request
-from jsonreading import extract_json
+from flask import Flask,render_template,request,json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    message = "Welcome to Doyenne"
-    return render_template("index.html", message=message)
+   message = "Hi!"
+   return render_template("hello.html", message=message)
 
-@app.route("/data")
-def data():
-    raw_data = extract_json()
-    current_user = str(raw_data ["users"][0])
-    return render_template("userprofile.html", current_user=current_user)
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-    if request.method == 'POST':
-        result = request.form
-        return render_template("result.html", result=result)
+
+
+@app.route('/confirmation', methods=["GET", "POST"])
+def confirmation():
+   data = {}
+
+   data["users"] = []
+   data["users"].append({
+       "name": request.args.get('email'),
+       "address": request.args.get('firstname'),
+       "phone": request.args.get('lastname')
+   })
+   with open("users.json", "w") as write_file:
+       json.dump(data, write_file)
+   return render_template('/confirmation.html')
+
 
 app.run(debug=True)
+
